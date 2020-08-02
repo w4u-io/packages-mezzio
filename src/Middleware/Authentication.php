d4e96844a3d2a548fdcf5891fbde7bd3f1d0025b
Olivier Jonet <olivier@w4u.be>
2020-08-02 19:45:03 +0200

    fix namespace

--- src/Middleware/Authentication.php
+++ src/Middleware/Authentication.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Olivier\Mezzio;
+namespace Olivier\Mezzio\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
